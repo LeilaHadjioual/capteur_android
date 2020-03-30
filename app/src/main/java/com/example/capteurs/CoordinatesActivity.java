87574baa21627b0df7d605ccd28a535a62dd749e
unknown <leila.hadjioual@gmail.com>
2020-03-30 20:47:06 +0200

    gps coordinate

--- app/src/main/java/com/example/capteurs/CoordinatesActivity.java
+++ app/src/main/java/com/example/capteurs/CoordinatesActivity.java
@@ -1,14 +1,145 @@
 package com.example.capteurs;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CoordinatesActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+    private TextView txtGps;
+    //    Context context = this;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinates);
+
+        txtGps = findViewById(R.id.txtGps);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+//        fetchLocation();
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+//                fetchLocation();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                return;
+            }
+        } else {
+        }
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
     }
+
+//    private void fetchLocation() {
+//        if (ContextCompat.checkSelfPermission(CoordinatesActivity.this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            // Permission is not granted
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(CoordinatesActivity.this,
+//                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+//
+//                new AlertDialog.Builder(CoordinatesActivity.this)
+//                        .setTitle("Permission localisation")
+//                        .setMessage("Vous devez accepter l'utilisation du gps pour utiliser cette fonctionnalité")
+//                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                ActivityCompat.requestPermissions(CoordinatesActivity.this,
+//                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+//                                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+//                            }
+//                        })
+//                        .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                dialog.dismiss();
+//                            }
+//                        })
+//                        .create()
+//                        .show();
+//
+//            } else {
+//                // No explanation needed, we can request the permission.
+//                ActivityCompat.requestPermissions(CoordinatesActivity.this,
+//                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+//                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+//            }
+//        } else {
+//            // Permission has already been granted
+//            fusedLocationProviderClient.getLastLocation()
+//                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                        @Override
+//                        public void onSuccess(Location location) {
+//                            if (location != null) {
+//                                Double latitude = location.getLatitude();
+//                                Double longitude = location.getLongitude();
+//                                txtGps.setText("latitude : " + latitude + "\nlongitude : " + longitude);
+//                            }
+//
+//                        }
+//                    });
+//        }
+//
+//
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Log.d("GPS", "onRequestPermissionsResult: ok");
+//            } else {
+//                Log.d("GPS", "onRequestPermissionsResult: false");
+//            }
+//        }
+//    }
 }
