4e14d4082129b6eaaac414b597b600ee373e3998
unknown <leila.hadjioual@gmail.com>
2020-04-01 15:53:05 +0200

    rework ques 1

--- app/src/main/java/com/example/capteurs/CoordinatesActivity.java
+++ app/src/main/java/com/example/capteurs/CoordinatesActivity.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -28,12 +29,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class CoordinatesActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     private TextView txtGps;
-    //    Context context = this;
-    private FusedLocationProviderClient fusedLocationProviderClient;
     private LocationManager locationManager;
-    private LocationListener locationListener;
 
 
     @Override
@@ -42,75 +39,51 @@ public class CoordinatesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_coordinates);
 
         txtGps = findViewById(R.id.txtGps);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        fetchLocation();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 15000, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    Double longitude = location.getLongitude();
+                    Double latitude = location.getLatitude();
+                    txtGps.setText("latitude : " + latitude + "\n" + "longitude : " + longitude);
+                }
 
-    }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    private void fetchLocation() {
-        if (ContextCompat.checkSelfPermission(CoordinatesActivity.this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                }
 
-            // Permission is not granted
-            if (ActivityCompat.shouldShowRequestPermissionRationale(CoordinatesActivity.this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                @Override
+                public void onProviderEnabled(String provider) {
 
-                new AlertDialog.Builder(CoordinatesActivity.this)
-                        .setTitle("Permission localisation")
-                        .setMessage("Vous devez accepter l'utilisation du gps pour utiliser cette fonctionnalité")
-                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(CoordinatesActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-                            }
-                        })
-                        .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
-                        .create()
-                        .show();
+                }
 
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(CoordinatesActivity.this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-            }
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
         } else {
-            // Permission has already been granted
-            fusedLocationProviderClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                Double latitude = location.getLatitude();
-                                Double longitude = location.getLongitude();
-                                txtGps.setText("latitude : " + latitude + "\nlongitude : " + longitude);
-                            }
-
-                        }
-                    });
+            ActivityCompat.requestPermissions(CoordinatesActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         }
-
-
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("GPS", "onRequestPermissionsResult: ok");
-            } else {
-                Log.d("GPS", "onRequestPermissionsResult: false");
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(getIntent());
+
+                } else {
+                }
+                return;
             }
+
         }
+
     }
 }
--- app/src/main/java/com/example/capteurs/DistanceActivity.java
+++ app/src/main/java/com/example/capteurs/DistanceActivity.java
@@ -2,13 +2,41 @@ package com.example.capteurs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DistanceActivity extends AppCompatActivity {
+
+    private TextView distTxt;
+    double latA = 17.4511252;
+    double latB = 17.4200480;
+    double lngA = 78.3748113;
+    double lngB = 78.4442193;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_distance);
+
+        distTxt = findViewById(R.id.distGps);
+
+        Location a = new Location("point A");
+
+        a.setLatitude(latA);
+        a.setLongitude(lngA);
+
+        Location b = new Location("point B");
+
+        b.setLatitude(latB);
+        b.setLongitude(lngB);
+
+        float distance = a.distanceTo(b);
+        distTxt.setText("distance " + distance/1000);
+
     }
+
+
 }
